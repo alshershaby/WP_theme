65e0a1639b194d8eb69f2b2874b4ba856f198436
Alshershaby <elshershaby93@gmail.com>
2025-04-13 06:38:38 +0200

    starting to create header footer and content

--- page.php
+++ page.php
@@ -1,13 +1,15 @@
 
 <?php
+
 get_header();
+
 while(have_posts() ){
 the_post();?>
-<h2> <a href="<?php the_permalink();?>"><?php the_title() ?> </a></h2>
+<h1>this is a page not a post</h1>
+<h2> <?php the_title() ?> </h2>
 <p> <?php the_content() ?> </p>
 <?php
 
 }
-
 
 get_footer();
--- single.php
+++ single.php
@@ -1,9 +1,9 @@
-
 <?php
 get_header();
+
 while(have_posts() ){
 the_post();?>
-<h2> <a href="<?php the_permalink();?>"><?php the_title() ?> </a></h2>
+<h2> <?php the_title() ?></h2>
 <p> <?php the_content() ?> </p>
 <?php
 
